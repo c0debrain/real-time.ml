cf3f1bfec37d9664f1fc0db1cfec8cd8b36a19cc
cpgeier <cpg3rb@virginia.edu>
2019-07-09 18:24:25 -0400

    Remove unused component

--- site/src/app/app.module.ts
+++ site/src/app/app.module.ts
@@ -5,14 +5,12 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MarketChartComponent } from './market-chart/market-chart.component';
-import { TweetTextComponent } from './tweet-text/tweet-text.component';
 import { TweetEmbedComponent } from './tweet-embed/tweet-embed.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MarketChartComponent,
-    TweetTextComponent,
     TweetEmbedComponent
   ],
   imports: [
--- site/src/environments/environment.prod.ts
+++ site/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  api_root_endpoint: "https://simple-twitter-stats.herokuapp.com"
 };
