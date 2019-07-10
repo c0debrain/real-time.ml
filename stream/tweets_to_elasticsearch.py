ebb3b1af236bd6a042552e4e51ad115cd13a7b10
cpgeier <cpg3rb@virginia.edu>
2019-07-09 20:41:25 -0400

    Add candidates to track twitter terms

--- stream/tweets_to_elasticsearch.py
+++ stream/tweets_to_elasticsearch.py
@@ -4,7 +4,7 @@ import twitter
 import pprint
 import requests
 import json
-
+import time
 CONSUMER_KEY = os.getenv("CONSUMER_KEY")
 CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
 ACCESS_TOKEN_KEY = os.getenv("ACCESS_TOKEN_KEY")
@@ -56,7 +56,18 @@ def delete_old():
     r = requests.post(ES_URI + ES_INDEX + '/_delete_by_query?conflicts=proceed', json=data)
     print("Deleted:", r.json()['_all']['total'])
 
-TRACK = ['election']
+TRACK = [
+    'election',
+    'joe biden',
+    'bernie sanders',
+    'kamala harris',
+    'elizabeth warren',
+    'donald trump',
+    'ben sasse',
+    'john kasich',
+    'ted cruz',
+    'mitt romney'
+]
 LANGUAGES = ['en']
 api = twitter.Api(consumer_key=CONSUMER_KEY,
                     consumer_secret=CONSUMER_SECRET,
@@ -65,18 +76,22 @@ api = twitter.Api(consumer_key=CONSUMER_KEY,
 
 count = 0
 initalize_es()
+t = time.perf_counter()
 for line in api.GetStreamFilter(track=TRACK, languages=LANGUAGES):
     status, response = add_to_elastic_search("twitter", line)
-
     if status:
         count += 1
-        if count % 10 == 0:
+        if count % 60 == 0:
+            elapsed_time = time.perf_counter() - t
+            t = time.perf_counter()
             r = requests.get(ES_URI + ES_INDEX + "/_stats/")
             es_size = r.json()['_all']['total']['store']['size_in_bytes']
             print(es_size)
             if es_size > ES_MAX_SIZE:
                 delete_old()
             print("Count:",count)
+            print("Elaspsed Time:",elapsed_time)
+            print("Throughput:",60/elapsed_time,"tweets per second")
     else:
         print(response)
         initalize_es()
